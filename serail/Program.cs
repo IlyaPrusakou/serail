3c8f8f6ef46bd89bc645aeaa42d9d04dd8224bb6
Ilya Prusakou <beyourself@tut.by>
2019-05-13 13:07:34 +0400

    Add project files.

---
+++ serail/Program.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Serialization;
+using System.Xml;
+
+namespace serail
+{
+    
+    
+    public class Song
+    {
+        [XmlElement("Title")]
+        public string Title;
+        [XmlElement("Duration")]
+        public int Duration;
+
+       
+        [XmlIgnore]
+        public string Lyrics;
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        
+        Song[] arr = new Song[]
+           {
+                new Song() {
+                    Title = "Title 1",
+                    Duration = 247,
+                    Lyrics = "Lyrics 1"
+                },
+                new Song() {
+                    Title = "Title 2",
+                    Duration = 456,
+                    Lyrics = "Lyrics 2"
+                }
+           };
+
+            XmlSerializer xmlSerializer = new XmlSerializer(arr.GetType());
+            string result;
+            using (StringWriter textWriter = new StringWriter())
+            {
+                xmlSerializer.Serialize(textWriter, arr);
+                result = textWriter.ToString();
+            }
+            Console.WriteLine(result);
+            Song[] songarray;
+            using (StringReader rdr = new StringReader(result))
+            {
+                songarray = (Song[])xmlSerializer.Deserialize(rdr);
+            }
+            Console.WriteLine(songarray.Length + "   " + songarray[0].Title + "  " + songarray[1].Title);
+            XmlWriterSettings set = new XmlWriterSettings();
+            set.Indent = true;
+            set.Encoding = Encoding.UTF8;
+            using (FileStream fs = new FileStream(@"D:\ДЗ\serial\serail\files\NNN.xml", FileMode.Create))
+            {
+                using (XmlWriter wrt = XmlWriter.Create(fs, set))
+                {
+                    xmlSerializer.Serialize(wrt, arr);
+                }
+            }
+            Song[] arrFromFile;
+            using (FileStream fs = new FileStream(@"D:\ДЗ\serial\serail\files\NNN.xml", FileMode.Open, FileAccess.ReadWrite))
+            {
+                using (XmlReader wrt = XmlReader.Create(fs))
+                {
+                   arrFromFile = (Song[])xmlSerializer.Deserialize(wrt);
+                }
+            }
+            foreach (Song item in arrFromFile)
+            {
+                Console.WriteLine(item.Title);
+            }
+            Console.ReadLine();
+        }
+    }
+}
